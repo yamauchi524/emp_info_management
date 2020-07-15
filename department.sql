41e722a07933994d8b5c48b38ee5ee22312c47d6
hyamauchi <hitomi.yamauchi@d2cr.co.jp>
2020-07-15 11:32:07 +0900

    change definition

--- department.sql
+++ department.sql
@@ -4,7 +4,7 @@
 部署名*/
 
 CREATE TABLE department(
-    department_id VARCHAR(255),
+    department_id INT AUTO_INCREMENT,
     department VARCHAR(255),
     department_register_date DATETIME DEFAULT CURRENT_TIMESTAMP, 
     department_update_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- image.sql
+++ image.sql
@@ -7,8 +7,9 @@
 画像データのファイル形式については、JPEG形式およびPNG形式のみとします。*/
 
 CREATE TABLE image(
-    image_id VARCHAR(255),
+    image_id INT AUTO_INCREMENT,
     image VARCHAR(255),
     image_register_date DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    image_update_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    image_update_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(image_id)
 );
